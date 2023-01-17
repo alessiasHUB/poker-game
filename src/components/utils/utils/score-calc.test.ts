78203267b36b09226b65d28c180fc7517e5ae621
Alessia B <borys.alessia@gmail.com>
2023-01-17 23:58:36 +0100

    test file added

---
+++ src/components/utils/utils/score-calc.test.ts
@@ -0,0 +1,22 @@
+import gameResult from "./score-calc";
+
+test("Check win by high card", () => {
+  expect(
+    gameResult({
+      Black: [
+        { cardValue: 2, cardKind: "H" },
+        { cardValue: 3, cardKind: "D" },
+        { cardValue: 5, cardKind: "S" },
+        { cardValue: 9, cardKind: "C" },
+        { cardValue: 13, cardKind: "D" },
+      ],
+      White: [
+        { cardValue: 2, cardKind: "C" },
+        { cardValue: 3, cardKind: "H" },
+        { cardValue: 4, cardKind: "S" },
+        { cardValue: 8, cardKind: "C" },
+        { cardValue: 14, cardKind: "H" },
+      ],
+    })
+  ).toEqual("");
+});
